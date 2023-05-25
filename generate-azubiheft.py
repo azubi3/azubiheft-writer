caba9c56cbd554d105164f0e582c95e8b8fab9d5
David S <d.stegh@supervista.de>
2023-05-25 09:48:02 +0200

    grammar

--- generate-azubiheft.py
+++ generate-azubiheft.py
@@ -7,7 +7,7 @@ generator = pipeline('text-generation', model=model_path, )
 
 while True:
     set_seed(randrange(99))
-    stichwort = input("Anfang des satzes: ")
+    stichwort = input("Anfang des Satzes: ")
     a = generator(stichwort, max_length=30, num_return_sequences=1, pad_token_id=generator.tokenizer.eos_token_id)
     generated_seq = a[0]['generated_text']
     sentence = generated_seq[:generated_seq.index('.')+1].strip()
