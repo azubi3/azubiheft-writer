34803098b58a63ed81576296dd421f84183cae5a
David S <d.stegh@supervista.de>
2023-05-26 14:32:24 +0000

    minimized reproducibility of text

--- generate-azubiheft.py
+++ generate-azubiheft.py
@@ -6,7 +6,7 @@ model_path = "model"
 generator = pipeline('text-generation', model=model_path, )
 
 while True:
-    set_seed(randrange(99))
+    set_seed(randrange(1000000))
     stichwort = input("Anfang des Satzes: ")
     a = generator(stichwort, max_length=30, num_return_sequences=1, pad_token_id=generator.tokenizer.eos_token_id)
     generated_seq = a[0]['generated_text']
