6e395df1bc7a66c7488c8be6c4d1f85503450595
David S <d.stegh@supervista.de>
2023-05-27 14:52:35 +0000

    Webui

--- generate-azubiheft.py
+++ generate-azubiheft.py
@@ -1,18 +1,30 @@
+import gradio as gr
 from transformers import pipeline, set_seed
 from random import randrange
 
 model_path = "model"
 
-generator = pipeline('text-generation', model=model_path, )
+generator = pipeline('text-generation', model=model_path)
 
-while True:
+def generate_sequence(stichwort):
     set_seed(randrange(1000000))
-    stichwort = input("Anfang des Satzes: ")
-    a = generator(stichwort, max_length=30, num_return_sequences=1, pad_token_id=generator.tokenizer.eos_token_id)
-    generated_seq = a[0]['generated_text']
-    try:
-        sentence = generated_seq[:generated_seq.index('.')+1].strip()
-        print(sentence)
-    except:
-        sentence = generated_seq[:generated_seq.index('\n')+1].strip()+"."
-        print(sentence)
+    a = generator(stichwort, max_length=30, num_return_sequences=5, pad_token_id=generator.tokenizer.eos_token_id)
+    sentence = None
+    iterator=0
+    while sentence is None:
+        try:
+            generated_seq = a[iterator]['generated_text']
+            sentence = generated_seq[:generated_seq.index('.')+1].strip()
+        except:
+            iterator+=1
+            if iterator > 5:
+                sentence = "Error, try again."
+    return sentence
+
+def my_function(stichwort):
+    return generate_sequence(stichwort)
+
+ui = gr.Interface(fn=my_function, inputs=[gr.Textbox(label="Anfang des Satzes")],
+                  outputs=[gr.Textbox(label="Generierter Bericht")], allow_flagging="never")
+
+ui.launch()
